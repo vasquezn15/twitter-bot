fbe089df1989291aa57ab982c90c291453bc088b
Nicolas Vasquez <51717304+vasquezn15@users.noreply.github.com>
2021-04-23 13:53:28 -0400

    UIRework call

--- src/Components/test.py
+++ src/Components/test.py
@@ -1,20 +1,29 @@
 import sys
 import json
 import requests
+import urllib.request
+url = 'http://localhost:5000/twitter/followers'
+req = urllib.request.Request()
+req.set_proxy('PROXY_HOST:PROXY_PORT', 'http')  # <---
+response = urllib.request.urlopen(req)
+data = json.loads(response.read().decode())  # <----
+print(data)
+# url = 'http://localhost:5000/bot/followers'
 
-url = 'http://localhost:5000/twitter/followers?user_id=1623840974'
+# r = requests.get(url)
+# r.set_proxy('PROXY_HOST:PROXY_PORT', 'http')
 
-r = requests.get(url)
-data = r.json()
+# data = r.json()
 
-print('PYTHON REQUEST')
+# print('PYTHON REQUEST')
 
-resp = {
-    "Message": "Hello from python",
-    "Data": data
-}
+# resp = {
+#     "Message": "Hello from python",
+#     "Data": data
+# }
+# print('r from python request' + r)
+# print(json.dumps(resp["Data"][i]["id"]))
+# # for i in range(len(resp["Data"])):
+# #     print(json.dumps(resp["Data"][i]["id"]))
 
-for i in range(len(resp["Data"])):
-    print(json.dumps(resp["Data"][i]))
-
-sys.stdout.flush()
+# sys.stdout.flush()
